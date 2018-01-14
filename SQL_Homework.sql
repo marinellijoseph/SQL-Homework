4d5c68b7948b63fdd0b488ffaf3df5f10108886b
marinellijoseph <marinellijoseph11@gmail.com>
2018-01-13 20:33:34 -0500

    saving changes

--- SQL_Homework.sql
+++ SQL_Homework.sql
@@ -19,7 +19,8 @@ FROM actor_name;
 DROP VIEW actor_name;
 
 
--- * 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
+-- * 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, 
+-- "Joe." What is one query would you use to obtain this information?
 SELECT *
 FROM actor
 WHERE first_name = "Joe ";
@@ -70,7 +71,24 @@ SELECT	*
 FROM actor;
 
 
--- 
+-- * 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT DISTINCT last_name, count(last_name)
+FROM actor
+GROUP BY last_name;
+
+
+-- * 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+SELECT DISTINCT last_name, count(last_name)
+FROM actor
+GROUP BY last_name;
+
+    
+-- * 4c. Oh, no! The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`,
+--  the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+  	
+    
+-- * 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query,
+--  if the first name of the actor is currently `HARPO`, change it to `GROUCHO`. Otherwise, change the first name to `MUCHO GROUCHO`, 
 
 
 
