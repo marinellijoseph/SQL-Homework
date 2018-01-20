e062c5a92e4f12c95173d16516e9774b4334242e
marinellijoseph <marinellijoseph11@gmail.com>
2018-01-20 15:13:49 -0500

    homework save

--- SQL_Homework.sql
+++ SQL_Homework.sql
@@ -126,13 +126,102 @@ GROUP BY staff.staff_id;
 
   	
 -- * 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
-
+SELECT DISTINCT film.title, count(actor_id)
+FROM film
+INNER JOIN film_actor ON
+film.film_id = film_actor.film_id
+GROUP BY film.film_id;
 
 
   	
 -- * 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
-
+SELECT count(film_id)
+FROM inventory
+WHERE film_id IN (
+	SELECT film_id
+    FROM film
+    WHERE title = 'Hunchback Impossible'
+    );
 
 
 -- * 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
+SELECT DISTINCT last_name, sum(amount)
+FROM customer
+INNER JOIN payment 
+ON customer.customer_id  = payment.customer_id
+GROUP BY customer.customer_id
+ORDER BY last_name;
 
+-- * 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, 
+-- films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries to display the titles of movies 
+-- starting with the letters `K` and `Q` whose language is English. 
+SELECT title
+FROM film
+WHERE 
+	title like "Q%" 
+	OR title like "K%" 
+    AND film_id IN (
+		SELECT film_id 
+        FROM film
+        WHERE language_id IN (
+			SELECT language_id
+            FROM language
+            WHERE name = 'English'
+		)
+	)
+;
+
+
+-- * 7b. Use subqueries to display all actors who appear in the film `Alone Trip`.
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN (
+	SELECT actor_id 
+    FROM film_actor
+    WHERE film_id IN (
+		SELECT film_id 
+        FROM film
+        WHERE title = 'Alone Trip'));
+        
+        
+-- * 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. 
+-- Use joins to retrieve this information.
+SELECT first_name, last_name, email
+FROM customer
+WHERE address_id IN ( 
+	SELECT address_id
+	FROM address
+	WHERE city_id IN (
+		SELECT city_id 
+		FROM city 
+		WHERE country_id IN (
+			SELECT country_id
+			FROM country
+			WHERE country = 'Canada')));
+            
+            
+-- * 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
+-- Identify all movies categorized as famiy films.
+SELECT title 
+FROM film 
+WHERE film_id IN (
+	SELECT film_id 
+    FROM film_category
+    WHERE category_id IN (
+		SELECT category_id 
+        FROM category 
+        WHERE category.name = 'familly'));
+
+
+-- * 7e. Display the most frequently rented movies in descending order.
+SELECT title
+FROM film
+ORDER BY rental_rate DESC;
+
+  	
+-- * 7f. Write a query to display how much business, in dollars, each store brought in.
+
+
+-- * 7g. Write a query to display for each store its store ID, city, and country.
+  	
+-- * 7h. List the top five genres in gross revenue in descending order. (**Hint**: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
